f287cc773eb01af8b2468d046cde96660fcc8cce
Martin Kucharek <kucharm2@gmail.com>
2020-09-07 10:17:51 -0400

    Interfaces

---
+++ Interfaces/src/Bicycle.java
@@ -0,0 +1,14 @@
+
+interface Bicycle {
+
+    //  wheel revolutions per minute
+    void changeCadence(int newValue);
+
+    void changeGear(int newValue);
+
+    void speedUp(int increment);
+
+    void applyBrakes(int decrement);
+    
+    void printStates();
+}
